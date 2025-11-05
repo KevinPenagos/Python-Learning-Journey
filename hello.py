f4d4057f213888d02fcbcf33ff14342f7f77b1cc
Kevin Penagos <kevinpenagosieb@gmail.com>
2025-11-05 10:30:11 -0500

    First commit: hello world and data type examples

---
+++ hello.py
@@ -0,0 +1,19 @@
+# Comment: Hello World
+print("Hello, World!")
+print('Hello, World!')
+
+
+"""
+This is a comment block.
+"""
+
+'''
+Another comment block. whith single quotes.
+'''
+
+# check the data type
+print(type("Hello, World!")) # This will print: <class 'str'>
+print(type(5))               # This will print: <class 'int'>
+print(type(3.14))            # This will print: <class 'float'>
+print(type(1 + 2j))         # This will print: <class 'complex'>
+print(type(True))            # This will print: <class 'bool'>
